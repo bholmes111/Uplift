cd8f6ca4a903da6d9d9bc75669668d078960fc69
bholmes111 <bholmes111@gmail.com>
2019-11-13 16:42:39 -0500

    FrequencyCRUD

---
+++ Uplift.DataAccess/Data/Repository/FrequencyRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    class FrequencyRepository : Repository<Frequency>, IFrequencyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FrequencyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public IEnumerable<SelectListItem> GetFrequencyListForDropDown()
+        {
+            return _db.Frequency.Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+        public void Update(Frequency frequency)
+        {
+            if(frequency != null)
+            {
+                var freqFromDb = _db.Frequency.FirstOrDefault(f => f.Id == frequency.Id);
+
+                freqFromDb.Name = frequency.Name;
+                freqFromDb.FrequencyCount = frequency.FrequencyCount;
+
+                _db.SaveChanges();
+            } 
+        }
+    }
+}
--- Uplift.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
+++ Uplift.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -8,6 +8,8 @@ namespace Uplift.DataAccess.Data.Repository.IRepository
     {
         public ICategoryRepository Category { get; }
 
+        public IFrequencyRepository Frequency { get; }
+
         void Save();
     }
 }
